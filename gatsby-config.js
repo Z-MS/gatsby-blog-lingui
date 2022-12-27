c1fe763410db2f45a36760c065ec4e544986bb7d
Zayyad Muhammad Sani <zayyadmuhammed700@gmail.com>
2022-12-27 13:48:18 +0100

    First commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,33 @@
  */
 module.exports = {
   siteMetadata: {
+    title: "Zayyad's blog",
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [],
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog`
+      }
+    },
+    "gatsby-plugin-mdx",
+    "gatsby-transformer-sharp",
+    {
+      resolve: `gatsby-theme-i18n`,
+      options: {
+        defaultLang: `en`,
+        configPath: require.resolve(`./i18n/config.json`),
+      }
+    },
+    {
+      resolve: `gatsby-theme-i18n-lingui`,
+      options: {
+        localeDir: `./i18n/lingui`,
+      },
+    }
+  ],
 }
