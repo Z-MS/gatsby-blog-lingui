33680b8743f89570fc8b5a10c11a32621aad53ed
Zayyad Muhammad Sani <zayyadmuhammed700@gmail.com>
2023-01-01 09:00:57 +0100

    Added more strings for translation

--- src/components/layout.js
+++ src/components/layout.js
@@ -27,9 +27,9 @@ const Layout = ({ pageTitle, children }) => {
 			<header className={siteTitle}>{data.site.siteMetadata.title}</header>
 			<nav>
 				<ul className={navLinks}>
-					<li className={navLinkItem}><Link to="/" className={navLinkText}>Home</Link></li>
-					<li className={navLinkItem}><Link to="/about" className={navLinkText}>About</Link></li>
-					<li className={navLinkItem}><Link to="/blog" className={navLinkText}>Blog</Link></li>
+					<li className={navLinkItem}><Link to="/" className={navLinkText}><Trans>Home</Trans></Link></li>
+					<li className={navLinkItem}><Link to="/about" className={navLinkText}><Trans>About</Trans></Link></li>
+					<li className={navLinkItem}><Link to="/blog" className={navLinkText}><Trans>Blog</Trans></Link></li>
 				</ul>
 			</nav>
 			<main>
